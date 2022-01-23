42f5c2745992d9691e2706c975899a1206d625a8
Navneet <navneet@kalabaaz.com>
2022-01-23 07:04:30 +0530

    Automated Commit on Deploy

--- src/lib/components/custom/workout/shareButton.svelte
+++ src/lib/components/custom/workout/shareButton.svelte
@@ -2,6 +2,8 @@
     import {page} from '$app/stores';
     import AppStore from '$lib/stores/appStore';
     import workout from '$lib/stores/workout/workout';
+    import { LocalStore } from '$lib/stores/storage';
+    import { EXERCISE_TYPE } from "$lib/stores/workout/types";
     import IconButton from '$lib/components/generic/iconButton.svelte';
     import Tooltip from '$lib/components/generic/Tooltip.svelte';
 
@@ -10,19 +12,29 @@
     let tooltipShow = false;
     
     const click = async () => {
-        workout.save(true);
-        const shareLink = {
-            title: `Sweat Workout`,
-            text: 'Here\'s the link to your saved workout.',
-            url: `${$page.url.origin}${$routineState.tabataLink}/?w=${$workout.meta.pathname}&`,
-        };
-        if ('canShare' in navigator && navigator.canShare(shareLink)) {
-            await navigator.share(shareLink);
-        } else if('clipboard' in navigator) {
-            await navigator.clipboard.writeText(shareLink.url);
-            if (!tooltipShow) {
-                tooltipBody = 'Link Copied!';
-                tooltipShow = true;
+        if (!$workout[EXERCISE_TYPE.EXERCISE].length) {
+            const lsWorkout = LocalStore.getItem('workout');
+            if (lsWorkout) workout.set(lsWorkout);
+        }
+
+        if (!$workout.meta.pathname && $workout[EXERCISE_TYPE.EXERCISE].length) {
+            workout.save(true);
+        }
+
+        if ($workout.meta.pathname) {
+            const shareLink = {
+                title: `Sweat Workout`,
+                text: 'Here\'s the link to your saved workout.',
+                url: `${$page.url.origin}${$routineState.tabataLink}/?w=${$workout.meta.pathname}&`,
+            };
+            if ('canShare' in navigator && navigator.canShare(shareLink)) {
+                await navigator.share(shareLink);
+            } else if('clipboard' in navigator) {
+                await navigator.clipboard.writeText(shareLink.url);
+                if (!tooltipShow) {
+                    tooltipBody = 'Link Copied!';
+                    tooltipShow = true;
+                }
             }
         }
     }
